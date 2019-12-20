fd919e65dd43750b596efc302efbf53a90045a93
Limba57 <limba@orange.fr>
2019-12-20 16:25:25 +0100

    debut continue in while loop

---
+++ main/BreakingOutWhileLoop.py
@@ -0,0 +1,21 @@
+import random
+
+playerHp = 260
+enemyAtkL = 60
+enemyAtkH = 80
+
+while playerHp > 0:
+    dmg = random.randrange(enemyAtkL, enemyAtkH)
+    playerHp = playerHp - dmg
+
+    if playerHp <= 30:
+        playerHp = 30
+
+    print("Enemy strick for", dmg, "point of damage.")
+    print("Current Hp are: ", playerHp)
+    print("-----")
+
+    if playerHp == 30:
+        print("You're Hp are low ...")
+        print("You have been teleport too a safer place")
+        break
--- main/PEPGuidelines.py
+++ main/PEPGuidelines.py
@@ -3,3 +3,26 @@ import sys
 from Test2 import InTest2
 
 
+def my_function(one, two,
+                  three, four,
+                  five, six):
+    print("hello word")
+
+
+def secound_function():
+    print("secund fonction")
+
+
+my_list = [1, 2,
+           3, 4,
+           5, 6]
+
+# ressembler les termes prioritaire entre eux
+x = 32*2 + 57*3
+
+check = True
+
+# revenir à la ligne après un :
+if check is True:
+    print("This is True")
+
